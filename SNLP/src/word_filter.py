13c72b3b924c84f96652b10a5de0a26c90c51c46
Joshua Peter Ebenezer <JoshuaEbenezer@users.noreply.github.com>
2017-11-30 07:53:13 +0530

    Update and rename zero_filter.py to word_filter.py

--- SNLP/src/word_filter.py
+++ SNLP/src/word_filter.py
@@ -3,7 +3,8 @@ import numpy as np
 import json
 df = pd.read_csv('/home/josh/python/SNLP/src/truncate_data/zero_filter/filtered.csv')
 j = len(df.index)
-df = df[df.word_count < 250]
+df = df[df.word_count < 500]
+df = df[df.word_count >10]
 
 out = df.to_json(orient='records')[1:-1].replace('},{', '}\n{')
 with open('word_filter.json', 'w') as f:
